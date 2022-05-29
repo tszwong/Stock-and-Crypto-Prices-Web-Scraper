270145f76be146567fd5352edcc7bca7d5fe116c
Tsz Kit Wong <wongt@bu.edu>
2022-05-29 01:28:09 -0400

    Update webScrap.py
    added comments & started initial scraping stage

--- webScrap.py
+++ webScrap.py
@@ -6,13 +6,27 @@ from datetime import date
 import requests
 import urllib.parse
 
-
+# creating url based on user input
 ticker = input("Enter Stock Symbol/Ticker (ex: AMZN) - ").upper()
 link = f"https://finance.yahoo.com/quote/{ticker}/?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&" \
        f"guce_referrer_sig=AQAAACUcyFsclu9rOyzDodJC1Cv2K0DOU0G4woBHTbUAxV0b0YnYVx35_g2_Kkh8C61IA4nySLZno0UVelD" \
        f"GvH57SGrTu5mXnkE5RbBN0xD-UxYOk-mKWiLJKR54HPtQXhL8QkOajVH3FxowIaW2lYPwJNSqkP9lvY7tDqeTA7ujnvu0"
 
+# link creation, requestion, and parse
 link_txt = urllib.parse.quote(link, safe="%:/?=&*+")
 page = requests.get(link_txt)
 soup = BeautifulSoup(page.content, "html.parser")
-print(link_txt)
+
+# searching in results for desired info
+results = soup.find("div", class_="D(ib) Mend(20px)")
+main_line = results.find_all("fin-streamer")
+price_info = {}
+for i in range(0,len(main_line)):
+       if i == 0:
+              price_info["curr_price"] = "$" + main_line[i].text.strip()
+       if i == 1:
+              price_info["daily_inc"] = main_line[i].text.strip()
+       if i == 2:
+              price_info["daily_per_inc"] = main_line[i].text.strip()
+
+print(price_info)
