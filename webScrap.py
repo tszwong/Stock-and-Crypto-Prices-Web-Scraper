b1f5da531487d20181fd3617cdf1b73b20727bf8
Tsz Kit Wong <wongt@bu.edu>
2022-05-31 14:13:31 -0400

    Update webScrap.py
    put link creation part into a separate function

--- webScrap.py
+++ webScrap.py
@@ -6,21 +6,31 @@ from datetime import datetime, date, timezone
 import requests
 import urllib.parse
 
-# creating url based on user input
-ticker = input("Enter Stock Symbol/Ticker (ex: AMZN) - ").upper()
-link = f"https://finance.yahoo.com/quote/{ticker}/?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&" \
-       f"guce_referrer_sig=AQAAACUcyFsclu9rOyzDodJC1Cv2K0DOU0G4woBHTbUAxV0b0YnYVx35_g2_Kkh8C61IA4nySLZno0UVelD" \
-       f"GvH57SGrTu5mXnkE5RbBN0xD-UxYOk-mKWiLJKR54HPtQXhL8QkOajVH3FxowIaW2lYPwJNSqkP9lvY7tDqeTA7ujnvu0"
 
-# link creation, requestion, and parse
-link_txt = urllib.parse.quote(link, safe="%:/?=&*+")
-page = requests.get(link_txt)
-soup = BeautifulSoup(page.content, "html.parser")
+price_info = {}
+
+
+def process_link(ticker):
+       """ creates a link based on user input 
+           which will be used for web scraping
+       """
+       
+       # creating url based on user input
+       link = f"https://finance.yahoo.com/quote/{ticker}/?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29v" \
+           f"Z2xlLmNvbS8&guce_referrer_sig=AQAAACUcyFsclu9rOyzDodJC1Cv2K0DOU0G4woBHTbUAxV0b0YnYVx35_g2_Kk" \
+           f"h8C61IA4nySLZno0UVelDGvH57SGrTu5mXnkE5RbBN0xD-UxYOk-mKWiLJKR54HPtQXhL8QkOajVH3FxowIaW2lYPwJNSq" \
+           f"kP9lvY7tDqeTA7ujnvu0"
+
+       # link creation, requestion, and parse
+       link_txt = urllib.parse.quote(link, safe="%:/?=&*+")
+       page = requests.get(link_txt)
+       soup = BeautifulSoup(page.content, "html.parser")
+       
+       return soup
 
 # searching in results for desired info
 results = soup.find("div", class_="D(ib) Mend(20px)")
 main_line = results.find_all("fin-streamer")
-price_info = {}
 price_info[ticker] = {}
 
 price_info[ticker]["curr_price"] = "$" + main_line[0].text.strip()
@@ -36,4 +46,6 @@ if curr_datetime > 20:
        price_info[ticker]["after_market_change"] = after_market_results[2].text.strip()
        price_info[ticker]["after_market_pct_change"] = after_market_results[3].text.strip('()')
 
+       
+ticker = input("Enter Stock Symbol/Ticker (ex: AMZN) - ").upper()
 print(price_info)
