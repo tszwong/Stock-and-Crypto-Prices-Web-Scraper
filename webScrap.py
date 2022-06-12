77f8b3c354b5c686cef07a97bae98718a3736982
Tsz Kit Wong <wongt@bu.edu>
2022-06-12 01:19:33 -0400

    Update webScrap.py
    changed from using yahoo finance web page to MarketWatch web page

--- webScrap.py
+++ webScrap.py
@@ -7,7 +7,7 @@ from datetime import datetime, date, timezone
 import requests
 import urllib.parse
 import sys
-
+import random
 
 # info that will be displayed to user
 price_info = {}
@@ -29,81 +29,89 @@ def clear():
 # function that does all the web scraping
 def find_price(soup, ticker):
     # searches for the specific tag containing key price information
-    results = soup.find("div", class_="D(ib) Mend(20px)")
-    main_line = results.find_all("fin-streamer")
+    results = soup.find("div", class_="intraday__data")
+    main_line = results.find_all("bg-quote")
+    # print(main_line)
 
-    # refining and stripping down the contents to the number values 
-    price_info[ticker] = {}  # create entry
+    # refining and stripping down the contents to the number values
+    price_info[ticker] = {}
+    price_info[ticker]["Time of Info"] = datetime.now().time().replace(microsecond=0)
     price_info[ticker]["Current Price"] = "$" + main_line[0].text.strip()
-    price_info[ticker]["Daily Change"] = main_line[1].text.strip()
-    price_info[ticker]["Daily Change %"] = main_line[2].text.strip('()')
+    price_info[ticker]["Daily Change"] = main_line[2].text.strip()
+    price_info[ticker]["Daily Change %"] = main_line[3].text.strip()
     stocks_list.append(ticker)  # adding the stock we searched to the list
 
-    try:
-        after_market_results = soup.find("div", class_="Fz(12px) C($tertiaryColor) My(0px) D(ib) Va(b)"). \
-            find_all("fin-streamer")
-        price_info[ticker]["Current After Market Price"] = "$" + after_market_results[1].text.strip()
-        price_info[ticker]["After Market Change"] = after_market_results[2].text.strip()
-        price_info[ticker]["After Markey Change %"] = after_market_results[3].text.strip('()')
-    
-    except AttributeError:
-        print("No after market price currently")
+    # try:
+    #     after_market_results = soup.find("div", class_="Fz(12px) C($tertiaryColor) My(0px) D(ib) Va(b)"). \
+    #         find_all("fin-streamer")
+    #     price_info[ticker]["After Market Price"] = "$" + after_market_results[1].text.strip()
+    #     price_info[ticker]["After Market Change"] = after_market_results[2].text.strip()
+    #     price_info[ticker]["After Market Change %"] = after_market_results[3].text.strip('()')
+    #
+    # except AttributeError:
+    #     print("No after market price currently")
 
 
 # helper function that creates a dynamic link for scraping
 def process_link(ticker):
-    
     # link creation
-    link = f"https://finance.yahoo.com/quote/{ticker}/"
+    link = f"https://www.marketwatch.com/investing/stock/{ticker}?mod=search_symbol/?9082?{random.randint(0,100000)}"
     link_txt = urllib.parse.quote(link, safe="%:/?=&*+")
+    # print(link_txt)
 
     # getting the html file contents
     page = requests.get(link_txt)
+    # print(page.content)
     soup = BeautifulSoup(page.content, "html.parser")
-
+    page.close()
     return soup
 
 
 # helper function that prints out info in a clear and readable way
-def display(item):
-    if type(item) == dict:
-        for key, value in price_info.items():
-            print("\nStock:", key)
-            for info in value:
-                print(info + ':', value[info])
-    else:
-        for i in range(1, len(item)+1):
-            print(i, item[i-1])
-    print()
+def display_price_info(item):
+    for key, value in item.items():
+        print("\nStock:", key)
+        for info in value:
+            print(info + ':', value[info])
+
+
+# helper function that prints out info in a clear and readable way
+def display_stock_list(item):
+    i = 1
+    for key in item:
+        print(f"{i}. {key}")
+        i += 1
+
+
+def refresh(item):
+    for stock in item:
+        find_price(process_link(stock), stock)
 
 
 # main function that deals with user input and interactions
 # calls the helper function to conduct scraping
 def client():
     while True:
-        ticker = input("Enter Stock Symbol/Ticker or stop (ex: AMZN) - ").upper()
+        ticker = input("Enter Stock Symbol/Ticker or Stop (ex: AMZN) - ").lower()
         clear()
-        
-        if ticker == "stop".upper():
+
+        if ticker == "stop":
             break
-        
+
         try:
             # initiates scraping
             soup = process_link(ticker)
-            for stock in price_info:
-                find_price(soup, stock)
             find_price(soup, ticker)
-            print(f"Prices as of {curr_time}")
-            display(price_info)
-            
+            display_price_info(price_info)
+
         except AttributeError:  # incase the ticker does not exist
             print("Invalid Ticker: stock does not exist\nPlease Try Again")
 
-        options_num = "123"
+        options_num = "1234"
         option = ""
         while True:
             choice = input(f"\n-- Menu --\n1. continue/add stock\n2. remove a stock\n"
-                           f"3. end program\nChoice: ")
+                           f"3. end program\n4. refresh current list\nChoice: ")
             clear()
             if choice in options_num:
                 option = choice
@@ -113,21 +121,25 @@ def client():
 
         if option == "3":  # end program
             break
-            
+
         elif option == "2":
             clear()
-            print(stocks_list)
-            remove_stock = int(input("Stock to remove:  "))
+            display_stock_list(price_info)
+            remove_stock = int(input("Stock to remove(enter index) :  "))
             price_info.pop(stocks_list[remove_stock-1])
-            stocks_list.remove(stocks_list[remove_stock-1])
             clear()
-            display(price_info)
-            
+            display_price_info(price_info)
+
         elif option == "1":
             clear()
             print("Current Watch List: ")
-            display(stocks_list)
+            display_stock_list(price_info)
             continue
+
+        elif option == "4":
+            refresh(price_info)
+            display_price_info(price_info)
+            print()
 
     clear()
     print("Program Ended")
