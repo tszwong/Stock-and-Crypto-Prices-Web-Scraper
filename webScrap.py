82d4c2911db74f8c4531ef5ac1686b7876214081
Tsz Kit Wong <wongt@bu.edu>
2022-06-13 17:38:17 -0400

    Update webScrap.py
    1. fixed small bugs in menu options function
    2. removed unnecessary code

--- webScrap.py
+++ webScrap.py
@@ -22,7 +22,7 @@ curr_time = f"{datetime.now().month}/{datetime.now().day}/{datetime.now().year} 
 
 def clear():
     """
-    clears the commandline
+    clears the terminal
     """
     if name == "nt":
         _ = system("cls")
@@ -30,8 +30,8 @@ def clear():
         _ = system("clear")
 
 
-# function that does all the web scraping
-def find_price(soup, ticker):
+# function that does all the scraping
+def find_price(soup, ticker, type):
     # searches for the specific tag containing key price information
     results = soup.find("div", class_="intraday__data")
     main_line = results.find_all("bg-quote")
@@ -40,32 +40,32 @@ def find_price(soup, ticker):
     # refining and stripping down the contents to the number values
     price_info[ticker] = {}
 
-    try:
-        open_or_aftermarket = soup.find("div", class_="element element--intraday")
-        status = open_or_aftermarket.find("div")
-        price_info[ticker]["Status"] = status.text.strip()
-    except AttributeError:
-        open_or_aftermarket = soup.find("div", class_="element element--intraday")
-        status = open_or_aftermarket.find("div")
-        price_info[ticker]["Status"] = status.text.strip()
+    # add open or closed market status to info section
+    if type == "stock":
+        try:
+            open_or_aftermarket = soup.find("div", class_="element element--intraday")
+            status = open_or_aftermarket.find("div")
+            price_info[ticker]["Status"] = status.text.strip()
+        except AttributeError:
+            open_or_aftermarket = soup.find("div", class_="element element--intraday")
+            status = open_or_aftermarket.find("div")
+            price_info[ticker]["Status"] = status.text.strip()
 
     price_info[ticker]["Time of Info"] = datetime.now().time().replace(microsecond=0)
     price_info[ticker]["Current Price"] = "$" + main_line[0].text.strip()
     price_info[ticker]["Change ($)"] = main_line[2].text.strip()
     price_info[ticker]["Change (%)"] = main_line[3].text.strip()
-    stocks_list.append(ticker)  # adding the stock we searched to the list
 
 
 # helper function that creates a dynamic link for scraping
-def process_link(ticker, type):
+def process_link(ticker):
     # link creation
     random_num = random.randint(0,100000)
     link = ""
-    if type == "stock":
-        link = f"https://www.marketwatch.com/investing/stock/{ticker}?mod=search_symbol/?9082" \
-               f"?{random_num}"
-    elif type == "cryptocurrency" or type == "crypto":
-        link = f"https://www.marketwatch.com/investing/cryptocurrency/{ticker}?{random_num}"
+
+    link = f"https://www.marketwatch.com/investing/stock/{ticker}?mod=search_symbol/?9082" \
+            f"?{random_num}"
+
     link_txt = urllib.parse.quote(link, safe="%:/?=&*+")
     # print(link_txt)
 
@@ -80,7 +80,7 @@ def process_link(ticker, type):
 # helper function that prints out info in a clear and readable way
 def display_price_info(item):
     for key, value in item.items():
-        print("\nStock:", key)
+        print("\nStock:", key.upper())
         for info in value:
             print(info + ':', value[info])
 
@@ -89,16 +89,18 @@ def display_price_info(item):
 def display_stock_list(item):
     i = 1
     for key in item:
-        print(f"{i}. {key}")
+        print(f"{i}. {key.upper()}")
         i += 1
 
 
-def refresh(item):
+# allows the user to refresh the entire list in their viewing
+def refresh(item, type):
     for stock in item:
-        find_price(process_link(stock), stock)
+        find_price(process_link(stock), stock, type)
 
 
-def menu_options():
+# menu that asks for user input to continue, end, or other options
+def menu_options(type):
     option = ""
     while True:
         choice = input(f"\n-- Menu --\n1. continue/add item\n2. remove an item\n"
@@ -113,7 +115,7 @@ def menu_options():
     if option == "4":  # end program
         return "break"
 
-    elif option == "2":
+    elif option == "2":  # removing an item
         clear()
         display_stock_list(price_info)
         remove_stock = input("Item to remove (enter ticker):  ")
@@ -122,22 +124,24 @@ def menu_options():
         display_stock_list(price_info)
         print()
 
-    elif option == "1":
+    elif option == "1":  # continue
         clear()
         print("Current Watch List: ")
         display_stock_list(price_info)
+        print()
         return "continue"
 
-    elif option == "3":
-        refresh(price_info)
+    elif option == "3":  # refresh
+        refresh(price_info, type)
         display_price_info(price_info)
         print()
 
 
 # main function that deals with user input and interactions
-# calls the helper function to conduct scraping
+# calls the helper functions to conduct scraping and displaying
 def client():
     while True:
+        type = ""
         while True:
             type = input("Cryptpocurrency or stock? Or enter stop to end program - ").lower()
             if type == "stock" or type == "crypto" or type == "cryptocurrency":
@@ -150,18 +154,18 @@ def client():
         ticker = input("Enter Symbol/Ticker (ex: AMZN) - ").lower()
         clear()
 
-        if type == "stop":
+        if ticker == "stop":
             break
 
         try:
             # initiates scraping
-            soup = process_link(ticker, type)
-            find_price(soup, ticker)
+            soup = process_link(ticker)
+            find_price(soup, ticker, type)
             display_price_info(price_info)
         except AttributeError:  # incase the ticker does not exist
             print("Invalid Ticker: item does not exist\nPlease Try Again")
 
-        execute_option = menu_options()
+        execute_option = menu_options(type)
         if execute_option == "break":
             break
         elif execute_option == "continue":
